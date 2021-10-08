4b1e06123d6ab342c250ac6bea7276f0006dc24d
Emanuel Gomez <emanueljgomez@gmail.com>
2021-10-08 16:55:42 -0300

    Created UI for create-user component

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,10 +1,64 @@
 import React, { Component } from "react";
 
 export default class CreateUser extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      username: "",
+    };
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value,
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const user = {
+      username: this.state.username,
+    };
+
+    alert(`El recurso ` + user + ` fue creado con éxito.`);
+
+    this.setState({
+      username: "",
+    });
+  }
+
   render() {
     return (
-      <div>
-        <p>CREATE USER COMPONENT</p>
+      <div style={{ width: 30 + "rem" }}>
+        <h3 style={{ margin: 1 + "vw" }}>Alta de nuevo recurso</h3>
+        <form onSubmit={this.onSubmit}>
+          {/* Employee name field */}
+          <div className="form-group" style={{ margin: 1 + "vw" }}>
+            <label style={{ marginBottom: 0.5 + "vw" }}>
+              Nombre y Apellido:
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+            />
+          </div>
+          {/* Submit button */}
+          <br />
+          <div className="form-group" style={{ margin: 1 + "vw" }}>
+            <input
+              type="submit"
+              value="Crear Recurso"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
       </div>
     );
   }
